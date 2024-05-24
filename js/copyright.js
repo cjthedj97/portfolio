fd2ed155a1df2829ba849c51f4ab66345a297524
CJ Saathoff <cjthedj97@users.noreply.github.com>
2024-05-24 17:49:03 -0400

    Delete js/copyright.js

--- js/copyright.js
+++
@@ -1,5 +0,0 @@
-function updateYear() {
-    var year = new Date().getFullYear();
-    var copyrightElement = document.getElementById("copyright");
-    copyrightElement.innerHTML = "&copy; " + year + " Christopher Saathoff";
-  }
